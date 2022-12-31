17971efb703ac4775e2f56714eeea85910f7d50f
Manthan Ank <manthan.ank46@gmail.com>
2022-12-31 10:19:45 +0530

    music player project added

---
+++ music player/script.js
@@ -0,0 +1,15 @@
+function play() {
+    var player = document.getElementById('player');
+    player.play();
+  }
+  
+  function pause() {
+    var player = document.getElementById('player');
+    player.pause();
+  }
+  
+  function skip() {
+    var player = document.getElementById('player');
+    player.currentTime = 0;
+    player.play();
+  }
