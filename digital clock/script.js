fdb37fe5d1eebb02ff889b8158bcad942280cf9f
Manthan Ankolekar <manthan.ank@gmail.com>
2023-01-02 12:11:32 +0530

    updated digital clock

--- digital clock/script.js
+++ digital clock/script.js
@@ -1,17 +1,17 @@
-function displayTime(){
+function displayTime() {
     var dateTime = new Date();
     var hrs = dateTime.getHours();
     var min = dateTime.getMinutes();
     var sec = dateTime.getSeconds();
     var session = document.getElementById('session');
 
-    if(hrs >= 12){
+    if (hrs >= 12) {
         session.innerHTML = 'PM';
-    }else{
+    } else {
         session.innerHTML = 'AM';
     }
 
-    if(hrs > 12){
+    if (hrs > 12) {
         hrs = hrs - 12;
     }
 
