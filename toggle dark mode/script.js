8e914bf61c621e02dc613fa9d672cbb373286bad
Manthan Ankolekar <manthan.ank@gmail.com>
2023-02-08 16:16:30 +0530

    updated

--- toggle dark mode/script.js
+++ toggle dark mode/script.js
@@ -1,3 +1,9 @@
+const toggleButton = document.querySelector(".toggle-button");
+
+toggleButton.addEventListener("click", function() {
+  this.classList.toggle("active");
+});
+
 function myFunction() {
     let element = document.body;
     element.classList.toggle("dark-mode");
